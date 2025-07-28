bbdcca1ff50db3c585ed5919515db5bf6aee8736
vero682 <8232300123@univermilenium.edu.mx>
2025-07-28 14:47:45 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
-self.addEventListener('install', evento => {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('mi-pwa-cache').then(cache => {
             return cache.addAll([
                 '/',
+                'albañileria.html',
+                'cocinera.html',
                 'index.html',
-                'fisica.html',
-                'fisica.jpeg'
+                'maternidad.html',
+                'looney-tunes.png',
+                'minioms.jpg',
+                'verdeee.jpg',
+                'estilos.css'
 
             ]);
         })
@@ -15,6 +20,6 @@ self.addEventListener('install', evento => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then(respuesta => respuesta || fetch(event.requets))
+        .then(respuesta => respuesta || fetch(event.request))
     );
 });
